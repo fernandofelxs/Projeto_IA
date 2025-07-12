cd005d7d8f44efa638f3bebe69d2241a76f6024f
fernandofelxs <fernando.felix@unifesp.br>
2025-07-12 18:08:58 -0300

    Add script to download the kaggle dataset

---
+++ scripts/kaggle.py
@@ -0,0 +1,8 @@
+# Download do dataset do Kaggle
+# Após o download, mover a pasta do dataset para o diretório root do projeto
+
+import kagglehub
+
+path = kagglehub.dataset_download("sumn2u/garbage-classification-v2")
+
+print("Path to dataset files:", path)
