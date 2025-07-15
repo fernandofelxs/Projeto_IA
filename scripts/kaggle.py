00f02ebe6f65a0e594cf1dea2f45653800e1ebcf
fernandofelxs <fernando.felix@unifesp.br>
2025-07-15 09:42:55 -0300

    Standardize the notebooks

--- scripts/kaggle.py
+++
@@ -1,8 +0,0 @@
-# Download do dataset do Kaggle
-# Após o download, mover a pasta /garbage-dataset/ para o diretório root do projeto
-
-import kagglehub
-
-path = kagglehub.dataset_download("sumn2u/garbage-classification-v2")
-
-print("Path to dataset files:", path)
